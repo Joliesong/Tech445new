55718132a02f0795e09fc0ba726d6d77a943ec13
Heather <lunajoliehb@gmail.com>
2021-04-28 19:55:57 -0400

    push

---
+++ devcontainers/project-templates/cpp/hello.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+int
+main ()
+{
+  std::vector<int> v;
+  for (int i : {5, 4, 3, 2, 1})
+  {
+    v.push_back (i);
+  }
+
+  for (auto val : v)
+  {
+    std::cout << val << ' ';
+  }
+
+  std::cout << "\nHello, World! My name is Heather." << std::endl;
+
+  return 0;
+}
